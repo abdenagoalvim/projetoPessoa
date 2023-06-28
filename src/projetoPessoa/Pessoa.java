52b28cab85ec17faec5474b9b1a6f9ad797ec403
Abdenago Alvim <abdenago.alvim@gmail.com>
2023-06-28 19:42:55 -0300

    Incluído os atributos, o construtror e getters e setters da classe Pessoa

--- src/projetoPessoa/Pessoa.java
+++ src/projetoPessoa/Pessoa.java
@@ -1,5 +1,24 @@
 package projetoPessoa;
 
 public class Pessoa {
-
+	private String nome;
+	private int idade;
+	private double peso;
+	private double altura;
+	public Pessoa(String nome, int idade) {
+		this.nome = nome;
+		this.idade = idade;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
 }
