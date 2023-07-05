de0e74d833088b57ab93e17767c857394661fa48
Abdenago Alvim <abdenago.alvim@gmail.com>
2023-07-05 15:51:45 -0300

    Incluído os métodos crescer, emagrecer, engordar, envelhecer e toString

--- src/projetoPessoa/Pessoa.java
+++ src/projetoPessoa/Pessoa.java
@@ -1,5 +1,7 @@
 package projetoPessoa;
 
+import java.text.DecimalFormat;
+
 public class Pessoa {
 	private String nome;
 	private int idade;
@@ -9,6 +11,7 @@ public class Pessoa {
 		this.nome = nome;
 		this.idade = idade;
 	}
+	
 	public double getPeso() {
 		return peso;
 	}
@@ -21,4 +24,31 @@ public class Pessoa {
 	public void setAltura(double altura) {
 		this.altura = altura;
 	}
+	public void crescer(double cm) {
+		this.altura += cm/100; //ajuste das unidades: a altura é espressa em metros e o crescimento em centímetros
+	}
+	public void emagrecer(double kg) {
+		this.peso -= kg;
+	}
+	public void engordar(double kg) {
+		this.peso += kg;
+	}
+	public void envelhercer(int anos) {
+		int idadeAnt = this.idade; //verificar quantos a pessoa deverá crescer
+		double cresc = 0.0; //calcular o valor de crescimento da pessoa
+		this.idade += anos;
+		if (this.idade <= 21) { //todos os anos envelhecidos irão gerar crescimento
+			cresc = 0.5 * anos;
+		} else if (idadeAnt < 21) { //só os anos envelhecidos até completar 21 anos irão gerar crescimento
+			cresc = 0.5 * (21 - idadeAnt);
+		}
+		this.crescer(cresc);
+	}
+
+	@Override
+	public String toString() {
+		DecimalFormat df = new DecimalFormat("0.00"); //formatar a altura com duas casas decimais
+		return "Pessoa [nome=" + nome + ", idade=" + idade + ", peso=" + peso + ", altura=" + df.format(altura) + "]";
+	}
+	
 }
