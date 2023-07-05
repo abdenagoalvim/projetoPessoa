e3a7c015f9cfc1c790a1c0ba6266d94dc0e49aac
Abdenago Alvim <abdenago.alvim@gmail.com>
2023-07-05 15:56:08 -0300

    Criado uma pessoa e testes dos métodos

--- src/projetoPessoa/Principal.java
+++ src/projetoPessoa/Principal.java
@@ -3,8 +3,22 @@ package projetoPessoa;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Pessoa p1 = new Pessoa("Marcolino", 17);
+		p1.setAltura(1.7);
+		p1.setPeso(70);
+		System.out.println(p1);
+		p1.crescer(2);
+		System.out.println(p1);
+		p1.engordar(5);
+		System.out.println(p1);
+		p1.emagrecer(3);
+		System.out.println(p1);
+		p1.envelhercer(2); //envelhece 2 anos e cresce 1cm
+		System.out.println(p1);
+		p1.envelhercer(4); //envelhece 4 anos e cresce 1cm - só até completar 21 anos
+		System.out.println(p1);
+		p1.envelhercer(2); //envelhece 2 anos e não cresce - já ultrapassou os 21 anos
+		System.out.println(p1);
 	}
 
 }
